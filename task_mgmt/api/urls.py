1709f9dcc436e577d685962a2611a24199da220a
thisgirlJo <judithogbole71@gmail.com>
2025-01-09 18:58:26 +0100

    Task Mgmt Api

--- task_mgmt/api/urls.py
+++ task_mgmt/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import UserRegistrationView, TaskViewSet, UserListView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 router = DefaultRouter()
 
@@ -11,7 +11,6 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('users/', UserListView.as_view(), name='users'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += router.urls
